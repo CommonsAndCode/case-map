ed336f08f462846394c8194f6badb37b0c1cb580
Sascha Mann <git@mail.saschamann.eu>
2026-02-25 22:37:32 +0100

    feat: add ConfigProvider React context
    - New src/app/ConfigContext.tsx with ConfigProvider and useConfig() hook
    - ConfigProvider parses URL params once via parseConfig() and provides
      the immutable AppConfig to the entire component tree
    - Wrap <App> in <ConfigProvider> in main.tsx
    - Components can now call useConfig() to access mode, dataUrl, theme
      overrides, chrome visibility flags, etc.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/style.css'
 import App from './App.tsx'
+import { ConfigProvider } from './app/ConfigContext.tsx'
 import "leaflet/dist/leaflet.css";
 import "./map/leafleticonfix";
 import "./app/i18n";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
 )
